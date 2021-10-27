d013e7dba6bc00d2fe4f0fc5a1966a9a992ddab7
mr-samsul-alam <contactsamsulalam@gmail.com>
2021-10-27 19:54:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const { request } = require('express');
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hiysg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('carMechanics');
+        const servicesCollection = database.collection('services')
+        //Get api
+        app.get('/services',async(req,res)=>{
+            const cursor =servicesCollection.find({})
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        //get Single Services
+        app.get('/services/:id',async (req,res)=>{
+            const id = req.params.id;
+            const query ={_id:ObjectId(id)}
+            const service =await servicesCollection.findOne(query);
+            res.json(service);
+        })
+        //Post Api
+        app.post('/services', async (req, res) => {
+            const service= req.body;
+            console.log('hit the post api', service);
+            const result = await servicesCollection.insertOne(service);
+
+             console.log(result);
+             res.json(result)
+            
+            // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        })
+
+    }
+    catch {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send("runing my crud server")
+})
+app.listen(port, () => {
+    console.log('Runinng Server on Port:', port);
+})
